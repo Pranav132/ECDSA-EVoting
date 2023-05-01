b88835198abe9dd01dc2b2e45fd744ec1d6c7671
Pranav132 <mais.06pranav@gmail.com>
2023-05-01 18:42:04 +0530

    fixed backend url

--- voting-client/src/Globals/constants.js
+++ voting-client/src/Globals/constants.js
@@ -1,4 +1,4 @@
 import { ec as EC } from "elliptic";
 
-export const backendURL = "http://schnorrevoting.pythonanywhere.com/"
+export const backendURL = "https://schnorrevoting.pythonanywhere.com/"
 export const ec = new EC("secp256k1");
